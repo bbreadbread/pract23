7cebc2f2b978a1d3d6bd88d708b77985cf0937a2
Mezenceva Polina <breadtt00@gmail.com>
2024-10-15 00:55:20 +0500

    pract23

---
+++ app/src/main/java/com/example/pract23/SIgnInActivity.kt
@@ -0,0 +1,32 @@
+package com.example.pract23
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+
+class SIgnInActivity : ComponentActivity() {
+    private lateinit var login: EditText
+    private lateinit var pass: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        login = findViewById(R.id.login)
+        pass = findViewById(R.id.pass)
+    }
+
+    fun next(view: View) {
+        if (login.text.toString().isNotEmpty() && pass.text.toString().isNotEmpty()) {
+            val intent = Intent(this, ResultActivity::class.java)
+            startActivity(intent)
+        }
+        else {
+            Toast.makeText(this, "Заполните оба поля", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
